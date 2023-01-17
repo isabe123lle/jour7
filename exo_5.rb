ca726364c0437af7a082f71ef573da891194bae4
isabe123lle <puechisabelle@free.fr>
2023-01-17 17:09:12 +0100

    rajout_5_6

---
+++ exo_5.rb
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+std::cout << "salut, je vais compter de 0 a 9." << std::end;
+int compteur {0};
+
+while 
+}
---
+++ exo_6.rb
@@ -0,0 +1,8 @@
+puts "bonjour,donne moi un nombre
+print">"
+iteration_number"gets.to_i
+increment=
+while (increment <iteration_number)
+    puts "bonjour toi !"
+    increment = increment +1
+end
